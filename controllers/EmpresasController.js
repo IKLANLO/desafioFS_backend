d99cc4f050a5c31f6da2b11cb05c1bb33682f072
IKLANLO <ilandaberea@yahoo.es>
2024-03-23 19:23:53 +0100

    añadido post empresas + validación de formato de email y de email ya utilizado

--- controllers/EmpresasController.js
+++ controllers/EmpresasController.js
@@ -3,7 +3,44 @@ const Empresas = require('../models/Empresas')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
+async function checkData(req, res) {
+  // comprueba si existe el dato que traemos por parámetro
+  // debe pasarse un objeto tipo { nombre_campo: req.body.nombre_campo }
+  try {
+    const check = await Empresas.findOne(req)
+    return check
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 const EmpresasController = {
+  async create(req, res) {
+    try {
+      // validamos que estén todos los campos requeridos
+      const data = req.body
+      if (!data.Nombre || !data.Sector || !data.Dirección || !data.Tamaño
+        || !data.Email || !data.Password) {
+          return res.status(401).send({ message: 'faltan datos' })
+        }
+      // validamos que el email no esté ya utilizado
+      await checkData({Email: req.body.Email})
+      // encriptamos el password
+      const password = bcrypt.hashSync(req.body.Password, 10)
+      const empresa = await Empresas.create({...req.body, Password: password})
+      res.status(201).send({ message: 'empresa creada correctamente', empresa})
+    } catch (error) {
+      console.log(error)
+      if (error?.code === 11000) return res.status(401)
+        .send({ message: 'el email ya existe, elige otro'})
+
+      if (error?.errors?.Email?.kind === 'regexp') return res.status(401)
+      .send({ message: 'formato de email erróneo'})
+
+      res.status(500).send({ message: 'error en la creación de la empresa'})
+    }
+  },
+
   async getAll(req, res) {
     try {
       /* 
@@ -15,7 +52,7 @@ const EmpresasController = {
       res.status(200).send(empresasData)
     } catch (error) {
       console.log(error)
-      res.status(500).send('error requesting data')
+      res.status(500).send({ message: 'error en la petición de datos' })
     }
   }
 }
--- routes/empresas.js
+++ routes/empresas.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const EmpresasController = require('../controllers/EmpresasController')
 
 router.get('/', EmpresasController.getAll)
+router.post('/create', EmpresasController.create)
 
 module.exports = router
